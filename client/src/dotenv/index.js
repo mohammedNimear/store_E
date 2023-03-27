ae609e41dc8bd67a09694850cf30c87926096884
mohammedNimear <mohammedalhassannumair@gmail.com>
2023-03-27 16:04:14 +0200

    stripe secret key

--- client/src/dotenv/index.js
+++ client/src/dotenv/index.js
@@ -1,6 +1,10 @@
-   export  const dot = {
-    REACT_API_TOKEN: 'a248c1cf8a5c43e2aaa6269813361a5dac01b4f9fd9f879c5b959a3b645915ad713c9aca70d7854b5bcdf26c98465abe14efc06c5bfb2279e5c4bfadabb84214f5ede8487504fdcef8aeb1623e2fc637ff0188feb77328b43a2ad87629a73e6dc2f2a63d822799d27d1a521bba78c5f6c567ee31a705155659d3d499ffcf746d',
-    REACT_API_URL: 'http://localhost:1337/api',
-    REACT_UPLOAD_URL: 'http://localhost:1337',
-}
- 
+export const dot = {
+  REACT_API_TOKEN:
+    "a248c1cf8a5c43e2aaa6269813361a5dac01b4f9fd9f879c5b959a3b645915ad713c9aca70d7854b5bcdf26c98465abe14efc06c5bfb2279e5c4bfadabb84214f5ede8487504fdcef8aeb1623e2fc637ff0188feb77328b43a2ad87629a73e6dc2f2a63d822799d27d1a521bba78c5f6c567ee31a705155659d3d499ffcf746d",
+  REACT_API_URL: "http://localhost:1337/api",
+  REACT_UPLOAD_URL: "http://localhost:1337",
+  STRAPI_SECRET_KEY:
+    "sk_test_51Mpv53LFGcbTMMKlJUQbZrBtFmmIXYU4mm3u9EieOPAyQ0zO6Zaoqe29QsUp0fLHkVDUchPDETZ1bIQ4MyEaAeOS00zFwxLvbc",
+  STRAPI_PUBLISHABLE_KEY:
+    "pk_test_51Mpv53LFGcbTMMKlFOlpdx9sS0KxFyjPelK6kHdJV4VWof1r9zzBbJOD6mpgOlvKpFkAOSDfgcHMFxmkAR3E8Usl0060DjwgWS",
+};
